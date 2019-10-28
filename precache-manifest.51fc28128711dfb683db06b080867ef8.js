ce5a0767793b13bbeeda5d06067cf13172a5a021
Mahi Mani <m.mahishamani@gmail.com>
2019-10-28 14:35:20 -0700

    Updates

---
+++ precache-manifest.51fc28128711dfb683db06b080867ef8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06eab4dba826413e7592267c2ac20914",
+    "url": "/Memory-Game/index.html"
+  },
+  {
+    "revision": "f23ee571ecc2f6e18d78",
+    "url": "/Memory-Game/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "dacbf2ad443e27800ec2",
+    "url": "/Memory-Game/static/js/2.02e6b8b4.chunk.js"
+  },
+  {
+    "revision": "f23ee571ecc2f6e18d78",
+    "url": "/Memory-Game/static/js/main.806fa3eb.chunk.js"
+  },
+  {
+    "revision": "53b474316436d0aed75f",
+    "url": "/Memory-Game/static/js/runtime-main.76f3846e.js"
+  },
+  {
+    "revision": "c3eecb02efaa13811070d42aac4e5a9e",
+    "url": "/Memory-Game/static/media/bg6.c3eecb02.jpg"
+  }
+]);
